a9b42abcf0ace65bb76801bb35472d3f7b0b982d
jaime <jaimesalvador.simental@gmail.com>
2016-06-16 13:28:33 -0500

    live reload detects new file

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -2,35 +2,34 @@ var gulp = require('gulp'),
   sass = require('gulp-sass'),
   cssnano = require('gulp-cssnano'),
   browserSync = require('browser-sync').create(),
-  pug = require("gulp-pug"),
-  plumber = require("gulp-plumber");
-  watch = require("gulp-watch");
-
-
+  pug = require('gulp-pug'),
+  plumber = require('gulp-plumber'),
+  watch = require('gulp-watch'),
+  src = './';
 
 gulp.task('styles', function(){
   gulp.src('./app/sass/main.sass')
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest('./app/dist/css'))
+    .pipe(gulp.dest(src + 'app/dist/css'))
     .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('views', function buildHTML() {
-  return gulp.src('./app/pug/index.pug')
+  return gulp.src(src + 'app/pug/index.pug')
   .pipe(plumber())
   .pipe(pug({
     pretty: true
   }))
-  .pipe(gulp.dest('./app/dist'))
+  .pipe(gulp.dest(src + 'app/dist'))
   .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('build-js', function(){
-  return gulp.src('./app/dist/scripts/*.js')
-    .pipe(watch('./app/dist/scripts/*.js'))
+  return gulp.src(src + 'app/dist/scripts/*.js')
+    .pipe(watch(src + 'app/dist/scripts/*.js'))
     .pipe(browserSync.reload({stream: true}));
 });
 
@@ -38,13 +37,13 @@ gulp.task('build-js', function(){
 gulp.task('serve', function() {
   browserSync.init({
     server: {
-      baseDir: './app/dist'
+      baseDir: src + 'app/dist'
     }
   });
-  gulp.watch('./app/sass/*.sass', ['styles']);
-  gulp.watch('./app/pug/*.pug', ['views']);
-  gulp.watch('./app/dist/scripts/*.js', ['build-js']);
-  gulp.watch('./app/dist/*.html').on('change', browserSync.reload);
+  gulp.watch('app/sass/*.{sass, scss}', {cwd: src}, ['styles']);
+  gulp.watch('app/pug/*.pug', {cwd: src}, ['views']);
+  gulp.watch('app/dist/scripts/*.js', {cwd: src}, ['build-js']);
+  gulp.watch('app/dist/*.html').on('change', browserSync.reload);
 });
 
 
